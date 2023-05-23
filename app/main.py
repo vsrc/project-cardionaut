826f5b68a9a0ab4ed73ee309039ce1043712fd7c
Vedran Stankovic <contact@vedrans.me>
2023-05-23 19:47:30 +0100

    streamlit

---
+++ app/main.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+
+
+def main():
+  st.set_page_config(
+    page_title="Project: Cardionaut - Heart Attack AI predictor",
+    page_icon=":broken_heart:",
+    layout="wide",
+    initial_sidebar_state="expanded"
+  )
+  
+  with open("assets/style.css") as f:
+    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+  
+  # input_data = add_sidebar()
+  
+  with st.container():
+    st.title("Project: Cardionaut")
+    st.write("Heart Attack AI predictor")
+  
+  # col1, col2 = st.columns([4,1])
+  
+
+
+ 
+if __name__ == '__main__':
+  main()
