835e59c8e9b00db332315378081f3e677bb88e3d
Vedran Stankovic <contact@vedrans.me>
2023-05-24 01:12:34 +0100

    updates

--- app/main.py
+++ app/main.py
@@ -85,8 +85,14 @@ def main():
     st.write("This is a simple AI model that predicts the probability of a heart attack based on a set of measurements.")
     st.write("The model is a simple Logistic Regression model with an accuracy of: 83.6%")
     
+    
 
-    add_predictions(input_data)
+    with st.expander("See result"):
+      add_predictions(input_data)
+
+    st.write("<h3>Disclaimer</h3", unsafe_allow_html=True)
+
+    st.write("<p>This app is for development and testing purposes only. The information provided is not intended to be a substitute for professional medical advice. Always consult with a healthcare professional before making any decisions about your health. The app may contain inaccuracies or errors. The app is not intended to be used for the diagnosis, treatment, or prevention of any medical condition. The app is not a medical device and should not be used as a substitute for medical care. The app is not intended to be used by children or adolescents. If you have any questions about your health, please consult with a healthcare professional.</p>", unsafe_allow_html=True)
   
 
 
